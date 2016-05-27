993b1bbf4aee1da97e9e77e0e5984a20a846154a
Huy Nguyen+Giang Nguyen <huy.nguyen+giang.nguyen@eastagile.com>
2016-05-27 11:08:06 +0700

    [f] increase base handler test coverage, add license

--- responsebot/handlers/event.py
+++ responsebot/handlers/event.py
@@ -1,3 +1,17 @@
+# Copyright 2016 Invisible Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 class BaseEventHandler(object):
     """
     Abstract event handler. Read more about event `here <../guides/user_event_handling.html>`_ and `here <https://dev.twitter.com/streaming/overview/messages-types#Events_event>`_.
--- tests/unit_tests/handlers/test_base.py
+++ tests/unit_tests/handlers/test_base.py
@@ -1,3 +1,17 @@
+# Copyright 2016 Invisible Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from unittest.case import TestCase
 
 from responsebot.common.exceptions import UserHandlerError
@@ -64,3 +78,13 @@ class BaseTweetHandlerTestCase(TestCase):
         client = MagicMock(config={'user_stream': True})
 
         self.assertRaises(UserHandlerError, HandlerWithErrorneousEventHandler, client=client)
+
+    def test_call_event_handler_handle(self):
+        client = MagicMock()
+        handler = Handler(client)
+        handler.event_handler=MagicMock()
+        event = MagicMock()
+
+        handler.on_event(event)
+
+        handler.event_handler.handle.assert_called_once_with(event)
